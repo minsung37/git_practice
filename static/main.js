2b69925503c72c9c70bfdc0b98fced467b32eb04
minsung37 <tampang99@naver.com>
2022-04-05 22:21:40 +0900

    review

--- static/main.js
+++ static/main.js
@@ -1,9 +1,13 @@
 // 민성 js 기능구현 1
 
-function() {
+function hello1() {
   alert("hi");
-  
-// 승준 js 기능구현 2
-function hello() {
-    alert('hello');
 }
+
+
+// 승준 js 기능구현 2
+function hell4o() {
+    alert("hello");
+}
+
+
